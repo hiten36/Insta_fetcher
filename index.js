bfcd44998d01574e8cd19a4ba5d37052bb69f1d3
hiten <hitenkhatri14@gmail.com>
2021-08-21 23:04:52 +0530

    3.1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const exphbs=require('express-handlebars');
 const path=require('path');
-
-
+const fs=require('fs');
+const is=require('scraper-instagram');
+const imageDownloader=require('image-downloader');
+const { urlencoded } = require('express');
 const app=express();
 const port=process.env.PORT || 8000;
-
+const ig=new is();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
@@ -14,12 +16,66 @@ app.set('view engine','handlebars');
 app.engine('handlebars',exphbs());
 
 app.get('/', async (req,res)=>{
-
+    fs.readdir('./static/images', (err, files) => {
+        if (err) console.log(err);
+        for (const file of files) {
+            fs.unlink(path.join('./static/images', file), err => {
+                if (err) console.log(err);
+            });
+        }
+    });
     res.render('index',{
         flag:false
     });
 })
-
+app.post('/',async (req,res)=>{
+    let name=req.body.name;
+    fs.readdir('./static/images', (err, files) => {
+        if (err) console.log(err);
+        for (const file of files) {
+            fs.unlink(path.join('./static/images', file), err => {
+                if (err) console.log(err);
+            });
+        }
+    });
+    try {
+        let details=await ig.getProfile(name);
+        const options = {
+            url: details.pic,
+            dest: './static/images/image.jpg'
+        }
+        imageDownloader.image(options)
+        .then(({ filename }) => {
+            res.render('index',{
+                flag1:true,
+                flag:true,
+                color:"success",
+                msg:"Success! ",
+                userName:details.name,
+                pic:details.pic,
+                bio:details.bio,
+                followers:details.followers,
+                following:details.following,
+                posts:details.posts
+            });
+        })
+        .catch((err) => {
+            res.render('index',{
+                flag1:true,
+                flag:false,
+                color:"danger",
+                msg:"Failed! Try again after sometime."
+            })
+        })
+    } catch (error) {
+        res.render('index',{
+            flag1:true,
+            flag:false,
+            color:"danger",
+            msg:"Failed! Try again after sometime."
+        })
+    }
+})
 
 app.listen(port,()=>{
     console.log('Listening...');
